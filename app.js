75dde949f0469f7a0b95f67f0b5a2843b630337a
NahSama <hoangnguyendinh1107@gmail.com>
2021-01-07 11:20:42 +0800

    Express Session Part 2

--- app.js
+++ app.js
@@ -43,36 +43,18 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
     console.log(req.session);
 
-    // if (!req.signedCookies.user) {
     if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if(!authHeader){
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
-
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var username = auth[0];
-        var password = auth[1];
-
-        if (username === 'admin' && password === 'password') {
-            // res.cookie('user', 'admin', {signed: true})
-            req.session.user = 'admin';
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
     } else {
-        // if (req.signedCookies.user === 'admin') {
-        if (req.session.user === 'admin') {
+        if (req.session.user === 'authenticated') {
             next();
         } else {
             var err = new Error('You are not authenticated!');
